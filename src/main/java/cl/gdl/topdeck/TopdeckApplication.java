8edbd71620b35fb37ff9ecf7ffd9cf658217928f
andgerald <geraldinebecerra93@gmail.com>
2025-04-02 15:24:29 -0300

    first commit

---
+++ src/main/java/cl/gdl/topdeck/TopdeckApplication.java
@@ -0,0 +1,13 @@
+package cl.gdl.topdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopdeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopdeckApplication.class, args);
+	}
+
+}
