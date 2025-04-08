e0188c9ad9e58b8f5205307be9695c6800c31207
Deimonlay13 <di.pizarrom@duocuc.cl>
2025-04-07 22:45:05 -0400

    primer commmit

---
+++ ms-stage/src/main/java/cl/gdl/ms_stage/MsStageApplication.java
@@ -0,0 +1,13 @@
+package cl.gdl.ms_stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStageApplication.class, args);
+	}
+
+}
