5d4359ec41e00442d0e8cfbf4f3b771c6137d281
GeraldineBecerra <gera.becerra@duocuc.cl>
2025-04-13 13:36:10 -0400

    Se devuelve las propiedades del config data,se agregan rutas del rol y se agrega contructor vacio a la dto

--- ms-rol/src/main/java/cl/gdl/ms_rol/dto/RolDTO.java
+++ ms-rol/src/main/java/cl/gdl/ms_rol/dto/RolDTO.java
@@ -9,12 +9,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "ROL")
 public class RolDTO {
 
